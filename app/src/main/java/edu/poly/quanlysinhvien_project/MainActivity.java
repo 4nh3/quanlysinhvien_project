5515406d104143f8b386973bba819fff40abc224
4nh <tranhanhle.2003@gmail.com>
2023-10-07 14:13:12 +0700

    first commit

---
+++ app/src/main/java/edu/poly/quanlysinhvien_project/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.poly.quanlysinhvien_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quanlysinhvien_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
